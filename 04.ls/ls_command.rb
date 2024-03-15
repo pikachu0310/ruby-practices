2bc0cc0109280bf3971481fe10c90e32dccc44fa
pikachu0310 <pikachu13711@gmail.com>
2024-03-15 15:49:09 +0900

    :sparkles: 出力する文字列の操作のロジック全体の実装

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -8,6 +8,7 @@ class LsCommand
   def initialize
     @options = {}
     parse_options
+    @files = DirectoryScanner.new(@options).scan(@directory)
   end
 
   def parse_options
@@ -19,12 +20,47 @@ class LsCommand
   end
 
   def run
-    scanner = DirectoryScanner.new(@options)
-    files = scanner.scan
     if @options[:l]
-      files.each { |file| puts file.details }
+      print_detailed_files(@files)
     else
-      puts files.map(&:path)
+      print_files_in_columns(@files.map { |file| File.basename(file.path) })
+    end
+  end
+
+  private
+
+  def print_files_in_columns(paths)
+    columns = 3
+    rows = (paths.length / columns.to_f).ceil
+    paths = paths.each_slice(rows).to_a
+    paths.last.fill(nil, paths.last.length...rows) if paths.last.length < rows
+
+    # 各列の最大幅を計算
+    column_widths = paths.map do |column|
+      column.compact.max_by(&:length).length + 2 # 最長ファイル名 + スペース2
+    end
+
+    paths.transpose.each do |row|
+      row.each_with_index do |file, index|
+        file ||= '' # nilの場合は空文字列に置き換え
+        printf "%-#{column_widths[index]}s", file
+      end
+      puts
+    end
+  end
+
+  def print_detailed_files(files)
+    widths = {
+      mode: 10, # "-rwxr-xr-x"
+      nlink: files.map { |f| f.stat.nlink.to_s.length }.max,
+      owner: files.map { |f| Etc.getpwuid(f.stat.uid).name.size }.max,
+      group: files.map { |f| Etc.getgrgid(f.stat.gid).name.size }.max,
+      size: files.map { |f| f.stat.size.to_s.size }.max,
+      mtime: 12, # "Dec 20 12:45"
+      filename: files.map { |f| File.basename(f.path).size }.max
+    }
+    files.each do |file|
+      puts file.details(widths)
     end
   end
 end
